824ed36b9b7cacf9d6038e25e1f8f3de917edf09
Manolis Vrondakis <manolisvrondakis@gmail.com>
2019-09-30 11:15:14 +0100

    a

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 module.exports = {
   APP_NAME : "cleaning-rota",
-  DOCUMENT_KEY : "1xc1WC8U8Ke-Stnv6vJsqZPqsvV9sNRh39V8Qs5NktIU",
-  PROCESS_TIME : 10 * 10000,
-  FB_USERNAME : "manolisisking@gmail.com",
-  FB_PASSWORD : "difficulttoguess",
-  CHAT_ID : "1810355172421120",
+  DOCUMENT_KEY : "",
+  PROCESS_TIME : 10 * 1000,
+  FB_EMAIL : "",
+  FB_PASSWORD : "",
+  CHAT_ID : "",
   GOOGLE_CREDENTIALS : {
-      "type": "service_account",
-      "project_id": "sheets-254120",
-      "private_key_id": "6d73eb8f66e1aa8f42d4bc1516755e5e26b89980",
-      "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQDzjXDHG1UcLOJV\nJM5E+60ATA2iAArTHiLR78U2Pp9734eDKdsZ6CrIutnMwmaLO7LDvzvGfG6qpd2k\nuNMyvlAbn1Ow1VQz5MqPbTo9FOeU0HvdT8tywgTLYJSKsstf2IVFHV2fcNdoTj0w\n2cStgMg9ubg5TKN3MduhbSRlsAhnwJRTziQxptsYZntUmduGWVnkvubkYbih4Fjr\nYalG8No3nIYl3xb11Sdc3VF2qAOfXTMlSPREpaLyM/3HwS501ckHK2WGquDlpEMu\njGpqKIRzpqS8Ve28WQ0EKgDFJWlWhmAhIQvhblQ7i9FGedtoI2ZKHZFjalcQ8ThV\n7cE7T0eHAgMBAAECggEAaC2LHQ/a8wdb9PGebbF9yc++URpVhwKwAD7gi5hz3ae6\nrmc7+jPuARizyzsoY8wwZjypk/hr2gDC8w+//0eauoYHZW+K6fumjX06WSBVArm/\nEeZs7vSA2oHAaCeciK30K6EpB+dbrJky4W91CoY4oPaND61xiq2/9afa1shagA3x\nenXPCESeV7EiVCLot0RuvCgpdsuxJpea8Oggmw5fzvNzyauKnnYvTzIx53TSonIa\ny7uTCUkheMlzvEpNpvwKUOiFQNrsK2Q8uLojMP8lzOMBPRHnDzy0OEfJh+DwoEga\n3/z+th7ZvcUutl0dikoUD0/CPb8RZEUfSc/WbZlUSQKBgQD+/07tdz+2X/9btyGi\nipoDsGEwOW0uL9xYz3PNfeXUO4ATzZ9t2O4ml4EFCz5vMzhmRS3ZWrNzqtQdHG6z\nBukqtSnWynszYM7oqvjDRqZDyOcPZZnWLGraqG3PBE78GCY2BNjxmAQQ0aNscvuV\n5vrPkcKtqnpAhcULelk4dsXVlQKBgQD0gpyDoPkkTCMy4/ebhrWJuPJehnPsxJ4e\nL3+je/o9ZTgZ3NyoLBmWeEpKAwtRRY3MhU+r7HSxk4catL8EgjWgdSzenoYKMEHj\nMcEmVExpYKb/pRPjBiXHk6/6MlhhhnCAGPDEHYMRPtUX5HP/r5udCTZWfW4nxddx\nS1QYk3HpqwKBgQDIL7LM5kNUb8TPQdPcASesIboK2+qEyfoYlrR47I+TsCp6YYD6\nAcXk8ifNx+f58KIkV8cIzhX3hnMDb6DQV2qRpqvz/f3+dc+6BQ9Kbpp0uLH8+mxt\nIW25v2SFkwJXErZczi1qbUcz1QCRUSuFoVHhxrKpGB7lrJ7mxeRpWYUyiQKBgQDk\nZJCQf6/ZUOlcIwdqFmXClKiI1Sy50FsmNRYT/r2b1c2WkvDmgfMQ06IJCljq/H5c\nuXXXURMbwINl1cnMC1jOXkWgWRE5uSQCdbpG9F0/3ZGAoJE38ckeaHzrx1U5G9xA\nO4vClRKV7Eweej5EaqOAB1fjaFyZ+DqrUELohenU6wKBgQC446vPHRbZGOAbYG9i\nccG9KBVqpYSrWI07NU1N12YGb463IaX3otEWbhJGzHpvWe80lVeKlo08MYl1HyFz\nOWyBVY+0NmiS2f27JvRsW16Sq3EbniDx3MqTaBe9EIPB+inRKiXx/kMmMdcdPLJB\nqGqogf07jwcifK1caszVtE/K5w==\n-----END PRIVATE KEY-----\n",
-      "client_email": "sheets@sheets-254120.iam.gserviceaccount.com",
-      "client_id": "114755525436274452008",
-      "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-      "token_uri": "https://oauth2.googleapis.com/token",
-      "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-      "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/sheets%40sheets-254120.iam.gserviceaccount.com"
+      "type": "",
+      "project_id": "",
+      "private_key_id": "",
+      "private_key": "",
+      "client_email": "",
+      "client_id": "",
+      "auth_uri": "",
+      "token_uri": "",
+      "auth_provider_x509_cert_url": "",
+      "client_x509_cert_url": ""
     }
 }
